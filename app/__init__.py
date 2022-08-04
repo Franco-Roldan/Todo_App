f418064424617c1852f445176257c65e2e3c365f
Franco_roldan <francoroldan018@gmail.com>
2022-08-04 19:26:25 -0300

    commit versión 1.2.1

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__)
     #configuración de las variables de entorno
     app.config.from_mapping(
-        SECRET_KEY="mikey",
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
         DATABASE_HOST=os.environ.get("FLASK_DATABASE_HOST"),
         DATABASE_PASSWORD=os.environ.get("FLASK_DATABASE_PASSWORD"),
         DATABASE_USER=os.environ.get("FLASK_DATABASE_USER"),
