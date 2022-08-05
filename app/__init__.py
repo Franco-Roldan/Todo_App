b2a13016534d76fcfb1c6f5ec9b38c1b99394390
Franco_roldan <francoroldan018@gmail.com>
2022-08-04 23:12:20 -0300

    arreglando errores 5

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ def create_app():
     app = Flask(__name__)
     #configuración de las variables de entorno
     app.config.from_mapping(
-        SECRET_KEY=os.environ.get("SECRET_KEY"),
-        DATABASE_HOST=os.environ.get("FLASK_DATABASE_HOST"),
-        DATABASE_PASSWORD=os.environ.get("FLASK_DATABASE_PASSWORD"),
-        DATABASE_USER=os.environ.get("FLASK_DATABASE_USER"),
-        DATABASE=os.environ.get("FLASK_DATABASE")
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE')
     )
     from . import db
    
