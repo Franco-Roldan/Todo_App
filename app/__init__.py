6c20651cccedfd7d170680df7444de4ff9e03b71
Franco_roldan <francoroldan018@gmail.com>
2023-05-08 11:27:28 -0300

    v_1.2.3_configuracion_de_variables_de_entorno

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
         DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PORT = os.environ.get('FLASK_DATABASE_PORT'),
         DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
         DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
         DATABASE=os.environ.get('FLASK_DATABASE')
